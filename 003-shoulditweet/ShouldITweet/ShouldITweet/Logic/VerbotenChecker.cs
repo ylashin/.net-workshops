972e90074fc1069a80a6482c8eaf732632c603b8
Yousry Mohamed <yousry.mohamed@readify.net>
2016-02-17 09:54:25 +1000

    Adding test project and initial for VerbotenChecker

---
+++ 003-shoulditweet/ShouldITweet/ShouldITweet/Logic/VerbotenChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShouldITweet.Logic
+{
+    public interface IVerbotenChecker
+    {
+        bool ValidateText(string text);
+    }
+    public class VerbotenChecker : IVerbotenChecker
+    {
+        public bool ValidateText(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 003-shoulditweet/ShouldITweet/ShouldITweetTests/VerbotenCheckerTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using ShouldITweet.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShouldITweetTests
+{
+    [TestFixture]
+    public class VerbotenCheckerTests
+    {
+        [TestCase("")]
+        [TestCase("        ")]
+        [TestCase(null)]
+        public void VerbotenChecker_WhenCheckingEmptyText_ShouldReturnTrue(string text)
+        {
+            VerbotenChecker checker = new VerbotenChecker();
+            var result = checker.ValidateText(text);
+            Assert.AreEqual(result, true);
+        }
+    }
+}
